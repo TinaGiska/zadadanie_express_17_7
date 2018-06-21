e617b27e49b921c0cc7beb95c535b302807956cc
Tina Giska <tina.tylicka@gmail.com>
2018-06-21 19:14:36 +0200

    Update task

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '1060219047032-g5kq2ohgcvoqa9uck0balm030chdsgbi.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET':'Zm8ctBqmgmHjEytwLINHSWer',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
